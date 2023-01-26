f6b3808e8b2ece9ad8952f639c7844a4524d5dce
victor-carancio <victor.carancio@gmail.com>
2023-01-26 18:34:15 -0300

    swagger documentation added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -39,8 +44,9 @@ app.use(xss());
 //routers
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("<h1>TODO Api</h1><a href='/api-docs'>Documentation</a>");
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/task", authenticateUser, taskRouter);
